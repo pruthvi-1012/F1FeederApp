19db63d82d8c373acfd8a797878f6dcfa1f85476
pruthvi-1012 <pruthvi.raj1012@gmail.com>
2015-11-03 10:19:40 +1100

    Introducing controller
    Introducing controller.js for navigation management

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,4 +1,7 @@
 angular.module('F1FeederApp.controllers', []).
+
+/* Drivers controller */
+
   controller('driversController', function($scope, ergastAPIservice) {
     $scope.nameFilter = null;
     $scope.driversList = [];
@@ -11,4 +14,20 @@ angular.module('F1FeederApp.controllers', []).
         //Dig into the responde to get the relevant data
         $scope.driversList = response.MRData.StandingsTable.StandingsLists[0].DriverStandings;
     });
+  }).
+
+/* Driver controller */
+
+   controller('driverController', function($scope,$routeParams, ergastAPIservice){
+    $scope.id = $routeParams.id;
+    $scope.races = [];
+    $scope.driver = null;
+
+    ergastAPIservice.getDriverDetails($scope.id).success(function(response){
+        $scope.driver = response.MRData.StandingsTable.StandingsLists[0].DriverStandings[0];
+    });
+
+    ergastAPIservice.getDriverRaces($scope.id).success(function (response) {
+        $scope.races = response.MRData.RaceTable.Races; 
+   });
   });
